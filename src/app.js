e4f336244da07a2bc55cf2e0db1b0df178bde9a1
kunalnew <kunalbharti929@gmail.com>
2024-10-06 23:33:35 +0530

    get post delete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express=require('express');
 const app=express();
 
-app.use('/',(req,res)=>{
-    res.send("hello at server")
-})
-
-app.use('/kaki',(req,res)=>{
+app.get('/kaki',(req,res)=>{
     res.send("hello kaki")
-})
+});
+
+app.post('/kaki',(req,res)=>{
+    res.send("posted hello at server")
+});
+
+app.delete('/kaki',(req,res)=>{
+    res.send("deleted hello at server")
+});
 
 app.listen(3000,()=>{
     console.log("server starting at port 3000....")
-})
+});
