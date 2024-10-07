10eec529622773e8befe1e165417e878dfdba871
kunalnew <kunalbharti929@gmail.com>
2024-10-07 19:34:56 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express=require('express');
 const app=express();
 
-app.get('/kaki',(req,res)=>{
-    res.send("hello kaki")
+app.use('/main',(req,res,next)=>{
+    console.log("first");
+    // res.send("hello kaki");
+    next();
+},(req,res,next)=>{
+     console.log("second");
+    //  res.send("second");
+     next();
+},(req,res)=>{
+    console.log("third");
+    res.send("third");
 });
 
-app.post('/kaki',(req,res)=>{
-    res.send("posted hello at server")
-});
 
-app.delete('/kaki',(req,res)=>{
-    res.send("deleted hello at server")
-});
 
 app.listen(3000,()=>{
     console.log("server starting at port 3000....")
