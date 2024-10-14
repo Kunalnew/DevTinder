d18b593c8134b4df5070bc61f7a77dd9502cbcaa
kunalnew <kunalbharti929@gmail.com>
2024-10-14 19:21:05 +0530

    basic APIs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const {auth}=require('./middleware/auth.js');
 const User = require('./models/user.js');
 app.use(express.json());
 
-app.post('/sign',async (req,res)=>{
+app.post('/sign',async (req,res)=>{  
  
     const user = new User(req.body);
     
@@ -18,6 +18,55 @@ app.post('/sign',async (req,res)=>{
     }
   
 });
+
+app.get('/user',async (req,res)=>{
+        const  userEmail=req.body.email;
+        try{
+          const user= await User.findOne({email:userEmail}).exec();
+          if(user.length===0){
+            res.status(404).send('user not found');
+          }
+          else {
+            res.send(user);
+          }
+        }
+        catch(err){
+           res.status(400).send("something went wwrong");
+        }
+})
+
+app.delete('/delete',async (req,res)=>{
+    const userID=req.body.userID;
+   try{
+      const user= await User.findByIdAndDelete(userID);
+      res.send("user deleted successfully");
+   }
+   catch(err){
+      res.status(400).send("error encountered");
+   }
+});
+
+app.patch('/update',async (req,res)=>{
+   const userID=req.body.userID;
+   const userData=req.body;
+  try{
+     const user= await User.findByIdAndUpdate(userID,userData);
+     res.send("user updated successfully");
+  }
+  catch(err){
+     res.status(400).send("error encountered");
+  }
+})
+
+app.get('/feed',async (req,res)=>{
+     try{
+        const user=await User.find({});
+        res.send(user);
+     }
+     catch(err){
+        res.status(400).send("something went wwrong");
+   }
+})
 
 connectDB()
    .then(()=>{
